3bc72b549dbb73a6176a2c17608a256e5c057ddc
Robert Nugent <d00244618@student.dkit.ie>
2024-11-27 20:10:49 +0000

    Add Like model and migration

---
+++ Models/Like.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SOA_CA2.Models
+{
+	public class Like
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Like_ID { get; set; }
+
+		[Required]
+		[ForeignKey("Post")] 
+		public int Post_ID { get; set; } 
+
+		[Required]
+		[ForeignKey("User")]
+		public int User_ID { get; set; } 
+
+		[Column(TypeName = "timestamp")]
+		public DateTime Created_At { get; set; } = DateTime.UtcNow; 
+	}
+}
--- Models/UserContext.cs
+++ Models/UserContext.cs
@@ -18,6 +18,7 @@ namespace SOA_CA2.Models
 		public DbSet<User> Users { get; set; }
 		public DbSet<Post> Posts { get; set; }
 		public DbSet<Comment> Comments { get; set; }
+		public DbSet<Like> Likes { get; set; }
 
 	}
 }
