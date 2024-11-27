15f84e1e9139c115999518940fff235a11f4d2b2
Robert Nugent <d00244618@student.dkit.ie>
2024-11-27 22:05:00 +0000

    Created Friend DTOs for reading, creating and updating a friendship.
    FriendCreateDto:
    This DTO is for creating a new friend for a user which will be sending a friend request to them.
    
    FriendUpdateDto:
    This DTO is for updating the friend request status (e.g. the request being accepted or rejected).

---
+++ Models/DTOs/Friend/FriendCreationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SOA_CA2.Models.DTOs.Friend
+{
+	// Used for creating a new friendship (e.g., sending a friend request)
+	public class FriendForCreationDto
+	{
+		public int Friend_User_ID { get; set; }  
+	}
+}
---
+++ Models/DTOs/Friend/FriendDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SOA_CA2.Models.DTOs.Friend
+{
+	// Standard read DTO for Friend relationship
+	public class FriendDto
+	{
+		public int Friend_ID { get; set; }
+		public int User_ID { get; set; }  
+		public int Friend_User_ID { get; set; }  
+		public required string Status { get; set; }
+	}
+}
