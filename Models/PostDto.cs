525d7565b4aae2feb10dfa17dfb616bc62a239fa
Robert Nugent <d00244618@student.dkit.ie>
2024-11-27 20:52:07 +0000

    Make standard Dto's for User and Post

---
+++ Models/PostDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SOA_CA2.Models
+{
+	public class PostDTO
+	{
+		public int Post_ID { get; set; }
+		public int User_ID { get; set; }
+		public required string Content { get; set; }
+		public string? Image_URL { get; set; }
+		public DateTime Created_At { get; set; }
+	}
+
+}
