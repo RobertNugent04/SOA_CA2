dd60c8351722fe1eae889075882d0f34739a1465
Robert Nugent <d00244618@student.dkit.ie>
2024-11-27 20:40:51 +0000

    Create Notification model and migration

---
+++ Migrations/20241127204019_AddNotificationTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SOA_CA2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNotificationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notification",
+                columns: table => new
+                {
+                    Notification_ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    User_ID = table.Column<int>(type: "integer", nullable: false),
+                    Message = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Created_At = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notification", x => x.Notification_ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notification");
+        }
+    }
+}
---
+++ Models/Notification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SOA_CA2.Models
+{
+	public class Notification
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+		public int Notification_ID { get; set; }
+
+		[Required]
+		[ForeignKey("User")] 
+		public int User_ID { get; set; } 
+
+		[Required]
+		[StringLength(255)] 
+		public required string Message { get; set; } 
+
+		[Column(TypeName = "timestamp")]
+		public DateTime Created_At { get; set; } = DateTime.UtcNow; 
+
+	}
+}
