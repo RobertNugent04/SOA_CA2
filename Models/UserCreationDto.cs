12d248726ae641254bfa09bfe2dd8cd9b56aa2ec
Robert Nugent <d00244618@student.dkit.ie>
2024-11-27 21:01:32 +0000

    Create User Dto's for logging in & creating and updating a user
    Created Dto's for three different scenarios and each Dto only has the fields required for that scenario.

---
+++ Models/UserCreationDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SOA_CA2.Models
+{
+	// Used for creating a new user
+	public class UserCreationDto
+	{
+		public required string Full_Name { get; set; }
+
+		public required string Username { get; set; }
+
+		public required string Email { get; set; }
+
+		public required string Password { get; set; }
+	}
+}
