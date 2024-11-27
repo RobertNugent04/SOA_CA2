957d401b5c94b87b503b19caa5c287edff318be3
Robert Nugent <d00244618@student.dkit.ie>
2024-11-27 21:55:03 +0000

    Create Like DTOs for reading and creating likes

---
+++ Models/DTOs/Like/LikeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SOA_CA2.Models.DTOs.Like
+{
+	// Standard read DTO for Like
+	public class LikeDto
+	{
+		public int Like_ID { get; set; }
+		public DateTime Liked_At { get; set; }
+		public int User_ID { get; set; }  
+	}
+}
