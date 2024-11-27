ff85ebbbbbd7586252a0f1929e69a6995b231695
Robert Nugent <d00244618@student.dkit.ie>
2024-11-27 21:50:13 +0000

    Create Comment DTOs for viewing, creating and updating comments

---
+++ Models/DTOs/Comment/CommentCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SOA_CA2.Models.DTOs.Comment
+{
+	//Used for creating a comment
+	public class CommentForCreationDto
+	{
+		public required string Content { get; set; }
+
+		public int Post_ID { get; set; } 
+	}
+}
---
+++ Models/DTOs/Comment/CommentUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SOA_CA2.Models.DTOs.Comment
+{
+	// Used for updating a comment
+	public class CommentForUpdateDto
+	{
+		public string? Content { get; set; }
+	}
+}
