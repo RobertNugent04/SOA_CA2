650114a3b3cd19bc4567eb2a9a0b2aa90331a9cc
Robert Nugent <d00244618@student.dkit.ie>
2024-11-27 22:16:03 +0000

    Add Notification DTOs for reading and creating notifications

---
+++ Models/DTOs/Notification/NotificationCreationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SOA_CA2.Models.DTOs.Notification
+{
+	// Used for creating a new notification
+	public class NotificationForCreationDto
+	{
+		public required string Message { get; set; }
+	}
+}
---
+++ Models/DTOs/Notification/NotificationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SOA_CA2.Models.DTOs.Notification
+{
+	// Standard read DTO for Notification
+	public class NotificationDto
+	{
+		public int Notification_ID { get; set; }
+		public required string Message { get; set; }
+		public DateTime Created_At { get; set; } = DateTime.UtcNow;
+	}
+}
