0f6ceabd470bc843e1c061c3e0cb0012b05366fb
Patrick Orjieh <orjiehpatrick@gmail.com>
2024-12-02 17:58:48 +0000

    Modify the User DTOs to login and update the user's details to include properties and make required attributes

--- vibez-webservice/Models/DTOs/User/UserLoginDto.cs
+++ vibez-webservice/Models/DTOs/User/UserLoginDto.cs
@@ -1,9 +1,22 @@
-﻿namespace SOA_CA2.Models.DTOs.User
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SOA_CA2.Models.DTOs.User
 {
-    // Login DTO
+    /// <summary>
+    /// DTO used for user login.
+    /// </summary>
     public class UserLoginDto
     {
-        public required string Username { get; set; }
+        /// <summary>
+        /// The username or email used for logging in.
+        /// </summary>
+        [Required]
+        public required string UsernameOrEmail { get; set; }
+
+        /// <summary>
+        /// The user's password.
+        /// </summary>
+        [Required]
         public required string Password { get; set; }
     }
 }
