4ee57acf82a92e1027453ca21fe2cac821d85cb0
Patrick Orjieh <orjiehpatrick@gmail.com>
2024-12-02 19:11:13 +0000

    Modify the Message Model to include the sender and receiver foreign keys and also check deleted messages and migrate to update

--- vibez-webservice/Models/DTOs/Message/MessageDto.cs
+++ vibez-webservice/Models/DTOs/Message/MessageDto.cs
@@ -1,15 +1,43 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using SOA_CA2.Models.DTOs.User;
-
-namespace SOA_CA2.Models.DTOs.Message
+﻿namespace SOA_CA2.Models.DTOs.Message
 {
-	// Standard read DTO for Message
-	public class MessageDto
-	{
-		public int Message_ID { get; set; }
-		public int Sender_User_ID { get; set; }
-		public int Receiver_User_ID { get; set; }
-		public required string Content { get; set; }
-		public DateTime Sent_At { get; set; } = DateTime.UtcNow;
-	}
+    /// <summary>
+    /// DTO used for transferring message data to the client.
+    /// </summary>
+    public class MessageDto
+    {
+        /// <summary>
+        /// The unique identifier of the message.
+        /// </summary>
+        public int MessageId { get; set; }
+
+        /// <summary>
+        /// The ID of the user who sent the message.
+        /// </summary>
+        public int SenderId { get; set; }
+
+        /// <summary>
+        /// The ID of the user who received the message.
+        /// </summary>
+        public int ReceiverId { get; set; }
+
+        /// <summary>
+        /// The content of the message.
+        /// </summary>
+        public required string Content { get; set; }
+
+        /// <summary>
+        /// Indicates whether the message has been deleted by the sender.
+        /// </summary>
+        public bool IsDeletedBySender { get; set; }
+
+        /// <summary>
+        /// Indicates whether the message has been deleted by the receiver.
+        /// </summary>
+        public bool IsDeletedByReceiver { get; set; }
+
+        /// <summary>
+        /// The timestamp when the message was created - UTC.
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+    }
 }
