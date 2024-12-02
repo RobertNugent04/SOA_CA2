5800d584ca9db1511090fd6fefe7681574bee890
Patrick Orjieh <orjiehpatrick@gmail.com>
2024-12-02 21:48:50 +0000

    Update the Model to add in Dto for Friendship and remove friends

--- vibez-webservice/Models/AppDbContext.cs
+++ vibez-webservice/Models/AppDbContext.cs
@@ -2,21 +2,18 @@
 
 namespace SOA_CA2.Models
 {
-	public class AppDbContext : DbContext
-	{
-		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-		{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
 
-		}
-
-
-		public DbSet<User> Users { get; set; }
-		public DbSet<Post> Posts { get; set; }
-		public DbSet<Comment> Comments { get; set; }
-		public DbSet<Like> Likes { get; set; }
-		public DbSet<Friend> Friend { get; set; }
-		public DbSet<Message> Message { get; set; }
-		public DbSet<Notification> Notification { get; set; }
-
-	}
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Friendship> Friendships { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+    }
 }
--- vibez-webservice/Models/DTOs/Friend/FriendCreationDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SOA_CA2.Models.DTOs.Friend
-{
-	// Used for creating a new friendship (e.g., sending a friend request)
-	public class FriendCreationDto
-	{
-		public int Friend_User_ID { get; set; }  
-	}
-}
