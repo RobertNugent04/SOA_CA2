cbe6e0205d8187a3cfb7237ed64f1dedc06743c6
Robert Nugent <d00244618@student.dkit.ie>
2024-12-03 00:02:41 +0000

    Add the home page route to the App

--- vibez-client/src/App.js
+++ vibez-client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Login } from "../src/features/login/Login.tsx";
+import { HomeRoute } from "./HomeRoute.tsx";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login/>} />
+        <Route path="/home" element={<HomeRoute />} />
       </Routes>
     </Router>
   );
