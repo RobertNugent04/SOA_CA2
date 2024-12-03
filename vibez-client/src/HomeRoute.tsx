a9cec08cf4826db1fea4bbe7d6c8ef069f8b9eb4
Robert Nugent <d00244618@student.dkit.ie>
2024-12-03 00:01:13 +0000

    Create Route for Home Page
    Create a route for the home page containing the navbar, posts, and messages components.

---
+++ vibez-client/src/HomeRoute.tsx
@@ -0,0 +1,21 @@
+// HomeRoute.tsx
+import React from 'react';
+import { Posts } from './features/posts/Posts.tsx';
+import { Navbar } from './features/navbar/Navbar.tsx';
+import { Messages } from './features/messages/Messages.tsx';
+
+export const HomeRoute: React.FC = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <div className="content-wrapper">
+        <div className="posts-container">
+          <Posts />
+        </div>
+        <div className="messages-container">
+          <Messages />
+        </div>
+      </div>
+    </div>
+  );
+};
