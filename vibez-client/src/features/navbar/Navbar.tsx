8b0c035208db82def29c82498c38f204159afb9d
Robert Nugent <d00244618@student.dkit.ie>
2024-12-04 19:21:02 +0000

    Add profile picture to navbar

--- vibez-client/src/features/navbar/Navbar.tsx
+++ vibez-client/src/features/navbar/Navbar.tsx
@@ -1,10 +1,10 @@
-// Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 import logo from '../../assets/images/vibez_logo.jpg';
 import bell from '../../assets/images/notification_bell.png';
 import search from '../../assets/images/search_icon.png';
+import profilePic from '../../assets/images/default_pfp.png'; // Import profile picture
 
 export const Navbar = () => {
   return (
@@ -21,6 +21,9 @@ export const Navbar = () => {
         <div className="navbar-search">
           <img src={search} alt="search icon" className="navbar-icon-image" />
           <input type="text" placeholder="Search" className="navbar-search-input" />
+        </div>
+        <div className="navbar-profile">
+          <img src={profilePic} alt="Profile" className="navbar-profile-pic" />
         </div>
       </div>
     </nav>
