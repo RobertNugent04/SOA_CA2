12017a22e877ac2a68e35dc75d3811e62510e956
Patrick Orjieh <orjiehpatrick@gmail.com>
2024-12-04 16:37:51 +0000

    Define and Implement the repository class for the business logic of the Messages

---
+++ vibez-webservice/Interfaces/IMessageRepository.cs
@@ -0,0 +1,37 @@
+﻿using SOA_CA2.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SOA_CA2.Interfaces
+{
+    /// <summary>
+    /// Interface for managing data access operations for the Message entity.
+    /// </summary>
+    public interface IMessageRepository
+    {
+        /// <summary>
+        /// Adds a new message to the database.
+        /// </summary>
+        Task AddMessageAsync(Message message);
+
+        /// <summary>
+        /// Retrieves all messages for a specific user.
+        /// </summary>
+        Task<IEnumerable<Message>> GetUserMessagesAsync(int userId);
+
+        /// <summary>
+        /// Retrieves all messages in a conversation between two users.
+        /// </summary>
+        Task<IEnumerable<Message>> GetConversationMessagesAsync(int userId, int friendId);
+
+        /// <summary>
+        /// Deletes a message by marking it as deleted for the sender or receiver.
+        /// </summary>
+        Task UpdateMessageAsync(Message message);
+
+        /// <summary>
+        /// Saves changes to the database.
+        /// </summary>
+        Task SaveChangesAsync();
+    }
+}
---
+++ vibez-webservice/Interfaces/IMessageService.cs
@@ -0,0 +1,32 @@
+﻿using SOA_CA2.Models.DTOs.Message;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SOA_CA2.Interfaces
+{
+    /// <summary>
+    /// Interface for managing business logic related to messages.
+    /// </summary>
+    public interface IMessageService
+    {
+        /// <summary>
+        /// Sends a message from one user to another.
+        /// </summary>
+        Task SendMessageAsync(int senderId, MessageCreationDto dto);
+
+        /// <summary>
+        /// Retrieves all messages for a specific user.
+        /// </summary>
+        Task<IEnumerable<MessageDto>> GetUserMessagesAsync(int userId);
+
+        /// <summary>
+        /// Retrieves all messages in a conversation between two users.
+        /// </summary>
+        Task<IEnumerable<MessageDto>> GetConversationMessagesAsync(int userId, int friendId);
+
+        /// <summary>
+        /// Deletes a message for the user.
+        /// </summary>
+        Task DeleteMessageAsync(int userId, int messageId);
+    }
+}
