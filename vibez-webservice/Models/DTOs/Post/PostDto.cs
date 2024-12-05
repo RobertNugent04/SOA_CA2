e0db452be6f935b899bd92eb67cd2000a8c66f85
Patrick Orjieh <orjiehpatrick@gmail.com>
2024-12-05 23:10:18 +0000

    Update the posts to include the user name and profile picture

--- vibez-webservice/Models/DTOs/Post/PostDto.cs
+++ vibez-webservice/Models/DTOs/Post/PostDto.cs
@@ -26,6 +26,16 @@
         public string? ImageUrl { get; set; }
 
         /// <summary>
+        /// The username of the user who created the post.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// The profile picture path of the user who created the post.
+        /// </summary>
+        public string? ProfilePicturePath { get; set; }
+
+        /// <summary>
         /// The timestamp when the post was created UTC.
         /// </summary>
         public DateTime CreatedAt { get; set; }
