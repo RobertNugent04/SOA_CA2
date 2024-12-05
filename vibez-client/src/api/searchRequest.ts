17721202346ff3b8d471444957f654a4a56a76c2
Robert Nugent <d00244618@student.dkit.ie>
2024-12-05 03:06:17 +0000

    Create searchApiRequest for search users by username

---
+++ vibez-client/src/api/searchRequest.ts
@@ -0,0 +1,26 @@
+const endpoint = 'http://localhost:5225/api/users/search';
+
+export const searchUserRequest = async (userName: string) => {
+  try {
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userName }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log('Search response:', data);
+      return data; 
+    } else {
+      const errorData = await response.json();
+      console.error('Search error:', errorData);
+      throw new Error('Search failed.');
+    }
+  } catch (error) {
+    console.error('Error during search request:', error);
+    throw error;
+  }
+};
